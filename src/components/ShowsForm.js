fa537c7d4ad018e49db4d218d96f935eb781ea3d
Christopher Breen <C.jbreen61@gmail.com>
2021-04-11 19:11:10 -0400

    added form to showsform

--- src/components/ShowsForm.js
+++ src/components/ShowsForm.js
@@ -7,8 +7,30 @@ class ShowsForm extends Component {
                 <h3>Add New Show </h3>
                 <form>
                     <div>
-                        <label>Title</label>
+                        <label>Title: </label>
+                        <input type="text" />
                     </div>
+                    <div>
+                        <label>Show Status: </label>
+                        <input type="text" />
+                    </div>
+                    <div>
+                        <label>Release Date: </label>
+                        <input type="date" />
+                    </div>
+                    <div>
+                        <label># Of Seasons: </label>
+                        <input type="number" />
+                    </div>
+                    <div>
+                        <label>Image: </label>
+                        <input type="text" />
+                    </div>
+                    <div>
+                        <label>Description: </label>
+                        <textarea></textarea>
+                    </div>
+                    <input type="submit" value="Add Show" />
                 </form>
                 
             </div>
