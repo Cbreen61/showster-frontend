0410e0be4f16d2d120abec8c0ba2a38dbe1cb2de
Christopher Breen <C.jbreen61@gmail.com>
2021-04-11 17:50:55 -0400

    added about,err,footer,nav,show,shows and showform files to components with stater class information

---
+++ src/components/ShowsForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ShowsForm extends Component {
+    render() {
+        return (
+            <div>
+                Form
+            </div>
+        )
+    }
+}
+
+export default ShowsForm
