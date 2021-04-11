d798784bfd62f3e1e044bc2c496e6eb1d1d8ff0b
Christopher Breen <C.jbreen61@gmail.com>
2021-04-11 17:35:26 -0400

    added thunk to index.js, created store and imported showsreducer.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import showsReducer from './reducers/showsReducer';
+
+const store = createStore(showsReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
