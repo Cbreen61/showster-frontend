65eb36c9a29fefba2977d93c2d9d30d46a527289
Christopher Breen <C.jbreen61@gmail.com>
2021-04-11 19:24:01 -0400

    added onchange, value to set state and name to associate the onchange function

--- src/components/ShowsForm.js
+++ src/components/ShowsForm.js
@@ -19,27 +19,27 @@ class ShowsForm extends Component {
                 <form>
                     <div>
                         <label htmlFor="title">Title: </label>
-                        <input type="text" id="title" />
+                        <input type="text" id="title" name="title" value={ this.state.title } onChange={ this.handleChange } />
                     </div>
                     <div>
                         <label htmlFor="status">Show Status: </label>
-                        <input type="text" id="status" />
+                        <input type="text" id="status" name="status" value={ this.state.status } onChange={ this.handleChange } />
                     </div>
                     <div>
                         <label htmlFor="release_date">Release Date: </label>
-                        <input type="date" id="release_date" />
+                        <input type="date" id="release_date" name="release_date" value={ this.state.release_date } onChange={ this.handleChange } />
                     </div>
                     <div>
                         <label htmlFor="seasons"># Of Seasons: </label>
-                        <input type="number" id="seasons" />
+                        <input type="number" id="seasons" name="seasons" value={ this.state.seasons } onChange={ this.handleChange } />
                     </div>
                     <div>
                         <label htmlFor="image">Image: </label>
-                        <input type="text" id="image" />
+                        <input type="text" id="image" name="image" value={ this.state.image } onChange={ this.handleChange } />
                     </div>
                     <div>
                         <label htmlFor="description">Description: </label>
-                        <textarea id="description"></textarea>
+                        <textarea id="description" name="description" value={ this.state.description } onChange={ this.handleChange } ></textarea>
                     </div>
                     <input type="submit" value="Add Show" />
                 </form>
