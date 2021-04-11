395186a82a4056ed93b91e73ba6cd1bbbfba902b
Christopher Breen <C.jbreen61@gmail.com>
2021-04-11 17:54:07 -0400

    added nav to app.js as well as router. added a home page to components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Nav from './components/Nav';
+
 
 
 class App extends Component {
   render(){
     return (
-      <div className="App">
-        <h1>Can you see me </h1>
-      </div>
+      <Router>
+        <Nav />
+        <div className="App">
+          <h1>Can you see me </h1>
+        </div>
+      </Router>
+
     );
   }
 }
