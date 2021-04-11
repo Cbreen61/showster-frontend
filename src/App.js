75ef23d43055cbfcec1852a6deeef9946f96814f
Christopher Breen <C.jbreen61@gmail.com>
2021-04-11 18:13:54 -0400

    added link and route too shows, imported sshows to app.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import Nav from './components/Nav';
 import Home from './components/Home';
-import ErrorPage from './components/Error'
 import About from './components/About';
+import Shows from "./components/Shows";
+import ErrorPage from './components/Error';
+
 
 
 
@@ -16,6 +18,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={ Home } />
           <Route exact path="/about" component={ About } />
+          <Route exact path="/shows" component={ Shows } />
           <Route component={ErrorPage} />
         </Switch>
         <Footer />
