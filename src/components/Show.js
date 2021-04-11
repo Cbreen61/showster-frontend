e5597f6734e2bb5e1d5106d49db0916058bc348a
Christopher Breen <C.jbreen61@gmail.com>
2021-04-11 18:50:58 -0400

    deconstructed show props in show.js

--- src/components/Show.js
+++ src/components/Show.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 class Show extends Component {
     render() {
+        const { title, status, realease_date, seasons, likes, dislikes, description, image } = this.props;
         return (
             <div>
-                Show
+                <h3>{ title }</h3>
+                <li>{ status }</li>
+                <li>{ realease_date }</li>
+                <li>{ seasons }</li>
+                <li>{ likes }</li>
+                <li>{ dislikes }</li>
+                <li>{ image }</li>
+                <p>{ description }</p>
             </div>
         )
     }
