10a5a52711dfdb6ca3eebe503240bd186963631f
Christopher Breen <C.jbreen61@gmail.com>
2021-04-11 19:18:08 -0400

    added htmlfor labels and id values to inputs so user can click on label to enter text box

--- src/components/ShowsForm.js
+++ src/components/ShowsForm.js
@@ -1,34 +1,45 @@
 import React, { Component } from 'react'
 
 class ShowsForm extends Component {
+    state = {
+        title: "",
+        status: "",
+        release_date: "",
+        seasons: "",
+        likes: "",
+        dislikes: "",
+        description: "",
+        image: ""
+    }
+
     render() {
         return (
             <div>
                 <h3>Add New Show </h3>
                 <form>
                     <div>
-                        <label>Title: </label>
-                        <input type="text" />
+                        <label htmlFor="title">Title: </label>
+                        <input type="text" id="title" />
                     </div>
                     <div>
-                        <label>Show Status: </label>
-                        <input type="text" />
+                        <label htmlFor="status">Show Status: </label>
+                        <input type="text" id="status" />
                     </div>
                     <div>
-                        <label>Release Date: </label>
-                        <input type="date" />
+                        <label htmlFor="release_date">Release Date: </label>
+                        <input type="date" id="release_date" />
                     </div>
                     <div>
-                        <label># Of Seasons: </label>
-                        <input type="number" />
+                        <label htmlFor="seasons"># Of Seasons: </label>
+                        <input type="number" id="seasons" />
                     </div>
                     <div>
-                        <label>Image: </label>
-                        <input type="text" />
+                        <label htmlFor="image">Image: </label>
+                        <input type="text" id="image" />
                     </div>
                     <div>
-                        <label>Description: </label>
-                        <textarea></textarea>
+                        <label htmlFor="description">Description: </label>
+                        <textarea id="description"></textarea>
                     </div>
                     <input type="submit" value="Add Show" />
                 </form>
