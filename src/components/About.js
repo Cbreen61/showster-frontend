944dcbeb0ab4cf9f38c53e1265d1afe52148149e
Christopher Breen <C.jbreen61@gmail.com>
2021-04-12 18:19:55 -0400

    updated about section

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ class About extends Component {
                 <Row className="justify-content-md-center">
                 <h2>About this Site</h2>
                 </Row>
-                <h5>This website is a place to look up information about your favorite shows, give them a thumbs up or down and add characters we may have missed.</h5>
+                <h5>This website is a place to look up information about your favorite shows and add any we may have missed.</h5>
 
                 
             </div>
