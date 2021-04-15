433b57396c2f5761c9b36cfdf0381b227806f6eb
Christopher Breen <C.jbreen61@gmail.com>
2021-04-15 19:41:30 -0400

    added styling to navigating links for shows

--- src/components/Shows.js
+++ src/components/Shows.js
@@ -1,17 +1,18 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { Nav } from 'react-bootstrap';
 
 
 const Shows = (props) => {
 
     return (
-        <div>
+        <Nav variant="pills" className="flex-column">
             <br></br>
             {props.shows.map(show =>
                 <li key={show.id}>
-                    <Link to={`/shows/${show.id}`}>{show.title}</Link>
+                    <Nav.Link href={`/shows/${show.id}`}>{show.title}</Nav.Link>
                 </li> )}
-        </div>
+        </Nav>
     )
 }
 
