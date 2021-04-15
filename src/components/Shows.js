1d45aeda179955a8d5f7b9867ed2323a3516a094
Christopher Breen <C.jbreen61@gmail.com>
2021-04-15 18:58:10 -0400

    updated shows.js to be a landing for links to individual shows

--- src/components/Shows.js
+++ src/components/Shows.js
@@ -1,30 +1,16 @@
-import React, { Component } from 'react'
-import { CardDeck, Row } from 'react-bootstrap';
-import { connect } from 'react-redux';
-import Show from './Show';
+import React from 'react'
 
-class Shows extends Component {
-    render() {
-        const shows = this.props.shows.map( (show, i) => <Show key={i} id={show.id} title={show.title} status={show.status} release_date={show.release_date} seasons={show.seasons}  description={show.description} image={show.image} />)
-        return (
-            <div>
-                
-                <Row className="justify-content-md-center">
-                    <h2> Shows </h2>
-                </Row>
-                <CardDeck>
-                    { shows }
 
-                </CardDeck>
-            </div>
-        )
-    }
+const Shows = (props) => {
+
+    return (
+        <div>
+            {props.shows.map(show =>
+                <li key={show.id}>
+                    <Link to={`/shows/${show.id}`}>{show.title}</Link>
+                </li> )}
+        </div>
+    )
 }
 
-const mapStateToProps = state => {
-    return {
-         shows: state.shows
-    }
-}
-
-export default connect(mapStateToProps)(Shows);
+export default Shows
